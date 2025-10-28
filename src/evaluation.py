6eeff0845b3feb79f6bec56426c801c1d98d83d6
HuuKhang117 <nguyenhuukhang1710hcm@gmail.com>
2025-10-28 21:14:41 +0700

    Update evaluation.py

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,1 +1,12 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def plot_model_results(results_df):
+    results_df.sort_values(by="RMSE", inplace=True)
+    sns.barplot(x=results_df.index, y=results_df["RMSE"])
+    plt.title("So sánh sai số RMSE giữa các mô hình")
+    plt.show()
+
+def get_best_model(results_df):
+    return results_df["RMSE"].idxmin()
 
