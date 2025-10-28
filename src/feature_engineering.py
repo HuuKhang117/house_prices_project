151ce2740f15b018c0110e51f5b7f65772678ae7
HuuKhang117 <nguyenhuukhang1710hcm@gmail.com>
2025-10-28 21:14:06 +0700

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,1 +1,6 @@
+def add_features(df):
+    df["TotalSF"] = df["TotalBsmtSF"] + df["1stFlrSF"] + df["2ndFlrSF"]
+    df["HouseAge"] = df["YrSold"] - df["YearBuilt"]
+    df["OutdoorSF"] = df["WoodDeckSF"] + df["OpenPorchSF"] + df["EnclosedPorch"] + df["ScreenPorch"] + df["PoolArea"]
+    return df
 
